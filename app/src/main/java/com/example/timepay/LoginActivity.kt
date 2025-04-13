74a2580f28fc1f8b0c836591049808a4176e4cf1
Eldar Lazhuyeu <elderblr2@gmail.com>
2025-04-13 20:30:48 +0200

    Login page improvements

--- app/src/main/java/com/example/timepay/LoginActivity.kt
+++ app/src/main/java/com/example/timepay/LoginActivity.kt
@@ -2,8 +2,12 @@ package com.example.timepay
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.timepay.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -13,6 +17,23 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Style the Sign Up text
+        val fullText = "Don't have an account? Sign Up"
+        val spannableString = SpannableString(fullText)
+        val signUpStart = fullText.indexOf("Sign Up")
+        val signUpEnd = signUpStart + "Sign Up".length
+        
+        // Apply purple color to "Sign Up"
+        val purpleColor = ContextCompat.getColor(this, R.color.purple_500)
+        spannableString.setSpan(
+            ForegroundColorSpan(purpleColor),
+            signUpStart,
+            signUpEnd,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        
+        binding.signUpText.text = spannableString
 
         binding.loginButton.setOnClickListener {
             val email = binding.emailInput.text.toString()
